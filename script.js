da32888c1bac2e0472fa691645a30413aa1f07d5
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-05-25 11:16:32 +0200

    Created the basci structure of the proyect

---
+++ script.js
@@ -0,0 +1,78 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const apiUrl = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium'
+
+const questionList = () =>{
+    fetch(apiUrl)
+ .then((res) => res.json())
+ .then((res) => console.log(res))
+ .catch((err) => console.error(err))
+}
+   
+
+let currentQuestionIndex
+
+function startGame() {
+ startButton.classList.add('hide')
+ currentQuestionIndex = 0
+ questionContainerElement.classList.remove('hide')
+ setNextQuestion()
+}
+
+function showQuestion(item) {
+    questionElement.innerText = item.question
+    item.answers.forEach((answer) => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+   
+    if (answer.correct) {
+        button.dataset.correct = true
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+    })
+}
+   
+function setNextQuestion() {
+    resetState()
+    showQuestion(questionList[currentQuestionIndex])
+}
+   
+function setStatusClass(element) {
+    if (element.dataset.correct) {
+      element.classList.add('color-correct')
+    } else {
+      element.classList.add('color-wrong')
+    }
+}
+
+function selectAnswer() {
+    Array
+   .from(answerButtonsElement.children)
+   .forEach((button) => { setStatusClass(button) })
+    if (questionList.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+}
+ 
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+   
+
+startButton.addEventListener('click', startGame)
+
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+ })
+ 
