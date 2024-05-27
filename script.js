2be853e513cc309d43c9a4ffa89c84ce0e110b88
DaniCabrera91 <danielcabreravazquez91@gmail.com>
2024-05-27 20:52:46 +0200

    show in DOM the first question

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 const apiUrl = 'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple'
 
+let dataItem;
+let current = 0;
+
+
 import {backupJSON} from './quiz.js'
 async function questionList() {
   //Hacer ternario para que en caso que la response sea undefined, coja el JSON local
@@ -12,16 +16,16 @@ async function questionList() {
     const res = await axios.get(apiUrl) 
     const saveData = res ? JSON.stringify(res.data.results) : JSON.stringify(backupJSON);
     localStorage.setItem("data", saveData)
-    const dataItem = JSON.parse(localStorage.getItem("data"))
+    dataItem = JSON.parse(localStorage.getItem("data"))
     console.log(dataItem);
-    return showQuestion(dataItem,0)
+    return showQuestion(dataItem, current)
     //startGame(resString)
   }catch (error) {
     console.error(error)
     localStorage.setItem("dataBackup", JSON.stringify(backupJSON))
+    return showQuestion(dataItem, current)
   }
 }   
-
 
 function startGame() {
 
@@ -31,22 +35,21 @@ function startGame() {
 }
 
 
-    function showQuestion(pato, current) {
-      console.log(pato[current].question)
-      // pato.forEach(element => {
-      //   console.log(element.question);
-      //   const questionElement = document.createElement('p');
-      //   questionElement.innerHTML += `${element.type} Hola` 
-      // });
-
+    function showQuestion(dataQuiz, current) {
+      const questionElement = document.createElement('p');
+      console.log(questionElement.innerHTML += `${dataQuiz[current].question}`) 
       const questionContainer = document.getElementById('question-container');
       questionContainer.appendChild(questionElement);
+
+      // answerButtonsElement = document.createElement('button');
+      // console.log(questionElement.innerHTML += `${dataQuiz[current].incorrect_answers}`) 
+      // questionContainer = document.getElementById('question-container');
+      // questionContainer.appendChild(questionElement);
     }
     function selectedAnswer(){
      //COMRPOBAR SI ES CORRECTO (HACEIS LO QUE TENGAIS QUE HACER)
     }
-  
-
+    
 startButton.addEventListener('click', startGame)
 
     //Pintar en pantalla una pregunta
